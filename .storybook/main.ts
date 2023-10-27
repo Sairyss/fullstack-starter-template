105f7fd9c916915a5a551f8fd8fd68ae9592e702
Joel Venable <joel.venable@hahparking.com>
2023-10-27 09:50:50 -0500

    chore: upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,16 @@
-import type { StorybookConfig } from '@storybook/core-common';
+import type { StorybookConfig } from '@storybook/types';
 
 export const rootMain: StorybookConfig = {
+  stories: [],
+  framework: {
+    name: '@storybook/react-vite',
+    options: {}
+  }
+
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
   //   // Return the altered config
   //   return config;
   // },
 };
+
