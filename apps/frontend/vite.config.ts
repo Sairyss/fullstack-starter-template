7c7983c1eb798d6d3e52bb8b6b47595f5acc7600
Joel Venable <joel.venable@hahparking.com>
2023-10-27 10:02:56 -0500

    chore: update dependencies

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   server: {
@@ -10,6 +11,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tsconfigPaths({ projects: ['./tsconfig.app.json']}),
     nxViteTsPaths(),
   ],
 
